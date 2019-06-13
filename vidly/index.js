a6e3e94af3f1554009bbcad5dc27dac74aa5147f
Romeo Enso <romeo.enso@viseo.com>
2019-06-13 16:28:06 +0800

    complete vidly project

--- vidly/index.js
+++ vidly/index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const winston = require('winston');
 const app = express();
 
+
 require('./startup/config')();
 require('./startup/connection')();
 require('./startup/logging')();
 require('./startup/middleware')(app);
 require('./startup/route')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  // Production Ready
+  require('./startup/prod')(app);
+}
 
 const port = process.env.PORT || 8001;
 
